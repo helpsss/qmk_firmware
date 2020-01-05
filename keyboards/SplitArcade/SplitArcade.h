e7be9077d4b8902a590de9707738ef7710368742
sbabborello <fire.shinobi87@gmail.com>
2020-01-05 13:19:47 -0500

    Update SplitArcade.h

--- keyboards/SplitArcade/SplitArcade.h
+++ keyboards/SplitArcade/SplitArcade.h
@@ -24,8 +24,6 @@
 #include "../../lib/lufa/LUFA/Drivers/Peripheral/SPI.h"
 
 
-#define constrain(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
-
 /* This a shortcut to help you visually see your layout.
  *
  * The first section contains all of the arguments representing the physical
